de758bc1bd587ff4d366550ae144e5bee92e7eef
Milkey Mouse <milkeymouse@team-ivan.com>
2016-07-20 23:51:18 -0700

    Get price data for the past 10 days

--- steam-bulk-sell.js
+++ steam-bulk-sell.js
@@ -13,9 +13,46 @@ for(var i=0;i<checkboxes.length;i++) {
     }
 }
 var cards = document.getElementsByClassName("itemHolder");
-for(var i=0;i<cards.length;i++) {
-    if(cards[i].style.display === "none"){continue;}
-    if(cards[i].classList.contains("disabled")){continue;}
-    //cards[i].click();
-    console.log(cards[i]);
+var card_idx = 0;
+//keep a sterile, callback-free version of this function
+var real_OnPriceHistorySuccess = SellItemDialog.OnPriceHistorySuccess;
+var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+function dateStringToTicks(dstring){
+    //takes a Steam date string and converts to ms since Unix epoch
+    var parts = dstring.split(" ");
+    var month = months.indexOf(parts[0]);
+    var year = parseInt(parts[2]);
+    var day = parseInt(parts[1]);
+    var hour = parseInt(parts[3].split(":")[0]);
+    var unixTime = Date.UTC(year, month, day, hour);
+    if(parts[3].split(":")[1] !== "")
+    {
+        unixTime += 1000*60*parseInt(parts[3].split(":")[1]);
+    }
+    //correct for specified timezone
+    if(parts.length > 4){
+        if(parts[4][0] === "+"){
+            unixTime += 1000*60*60*parseInt(parts[4].substring(1));
+        } else if(parts[4][0] === "-"){
+            unixTime -= 1000*60*60*parseInt(parts[4].substring(1));
+        }
+    }
+    return unixTime;
+}
+
+function sellNextCard(){
+    while(cards[card_idx].style.display === "none" && card_idx<cards.length){card_idx++;}
+    var elItem = cards[card_idx].children[0];
+    g_ActiveInventory.SelectItem(null, elItem, elItem.rgItem, false);
+    //monkey-patch the XMLHTTPRequest result for the price data
+    SellItemDialog.OnPriceHistorySuccess = function(transport){
+        for(var i=transport.responseJSON.prices.length;i>0;i--){
+            if(Date.now() - dateStringToTicks(transport.responseJSON.prices[i][0]) > 1000*60*60*24*10){continue;} //ignore data older than 10 days
+            console.log(transport.responseJSON.prices[i][0]);
+        }
+        real_OnPriceHistorySuccess(transport);
+        SellItemDialog.OnPriceHistorySuccess = real_OnPriceHistorySuccess;
+    } 
+    SellCurrentSelection();
 }
